34388f74a5d0f7b57bdb2a28e4db51bcc0e60ecd
Jahiruddin Rana <jahiruddinrana57@gmail.com>
2022-07-13 12:10:54 +0600

    update all

--- js/main.js
+++ js/main.js
@@ -4,3 +4,23 @@ $(document).ready(function(){
   });
 });
 
+
+
+const nav = document.querySelector('nav');
+const main_content = document.querySelector('.body-content');
+
+const options = {
+  rootMargin: '-11%'
+};
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      nav.classList.remove('changeBackground');
+    } else {
+      nav.classList.add('changeBackground');
+    }
+  })
+}, options)
+
+observer.observe(main_content)
